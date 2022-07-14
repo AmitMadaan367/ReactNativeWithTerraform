8fab42ea133e65f2ac04391e1cf21007cc1c9dcf
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-07-14 19:56:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+
+
 resource "aws_s3_bucket" "mycodepipeline2" {
   bucket = "my-terraform-first-bucket2"
 
